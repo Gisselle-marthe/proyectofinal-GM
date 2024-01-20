6445b754a78f1a1b1747ab114097c3e5196e6c58
Gisselle Marthe <gissellemarthe@gmail.com>
2024-01-19 20:59:02 -0500

    Proyecto final, primer commit

---
+++ src/app/layouts/dashboard/pages/teach/teach.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teach',
+  templateUrl: './teach.component.html',
+  styleUrl: './teach.component.scss'
+})
+export class TeachComponent {
+
+}
---
+++ src/app/layouts/dashboard/pages/teach/teach.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeachComponent } from './teach.component';
+
+
+
+@NgModule({
+  declarations: [
+    TeachComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeachModule { }
